5b2306e9c9edfa575b34f6d3161968c808015fe3
shilaj-10 <57117045+shilaj-10@users.noreply.github.com>
2019-11-18 18:29:33 -0500

    ad playlist.py

---
+++ unit-3/homework/playlist.py
@@ -0,0 +1,16 @@
+# Problem 3
+
+playlist = [{'title': 'Bodak Yellow', 'genre': 'Pop', 'artiste': 'Cardi B','year': '2017', 'length': 3.25},
+{'title': 'writing on the wall', 'genre': 'hip Pop', 'artiste':'French Montana','year': '2019', 'length': 3.31},
+{'title': 'Mi Gente', 'genre': 'Reggaeton', 'artiste': 'J Balvin','year': '2017', 'length': 3.06},
+{'title': 'La Modelo', 'genre': 'Reggaeton', 'artiste': 'Ozuna','year': '2018', 'length': 4.16},
+{'title': 'Zone', 'genre': 'Rap', 'artiste': 'Arizona Zervas','year': '2017', 'length': 3.21},
+{'title': 'One I Want', 'genre': 'Pop', 'artiste': 'Majid Jordan','year': '2017', 'length': 3.33},
+{'title': 'No Strings', 'genre': 'Rap', 'artiste': 'Kid Ink','year': '2017', 'length': 3.34},
+{'title': 'Needed You', 'genre': 'Trap', 'artiste': 'Illenium','year': '2017', 'length': 5.06},
+{'title': 'Say It', 'genre': 'Trap', 'artiste': 'Flume','year': '2016', 'length': 4.27},
+{'title': 'Finest Hour', 'genre': 'Trap', 'artiste': 'Cash Cash','year': '2018', 'length': 4.05}]
+
+
+print (playlist.values(title))
+
